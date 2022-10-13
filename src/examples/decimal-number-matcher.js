5979f79d3b05b0c47e1f08053c385efd7e8019f9
Ondrej Hlusicka <ondrej.hlusicka@plus4u.net>
2022-10-13 09:58:25 +0200

    refactor error handling

--- src/examples/decimal-number-matcher.js
+++ src/examples/decimal-number-matcher.js
@@ -22,6 +22,26 @@ class DecimalNumberMatcher {
   }
 
   match(value) {
+    const ERRORS = {
+      INVALID_DECIMAL: {
+        code: "doubleNumber.e001",
+        message: "The value is not a valid decimal number."
+      },
+      DIGITS_EXCEEDED: {
+        code: "doubleNumber.e002",
+        message: "The value exceeded maximum number of digits."
+      },
+      DECIMALS_EXCEEDED: {
+        code: "doubleNumber.e003",
+        message: "The value exceeded maximum number of decimal places."
+      }
+    }
+
+    function throwError(error) {
+      result.addInvalidTypeError(error.code, error.message);
+      return result;
+    }
+
     const DEFAULT_MAX_DIGITS = 11;
     const maxDigits = this.params[0] || DEFAULT_MAX_DIGITS;
     const maxDecimals = this.params[1];
@@ -36,16 +56,13 @@ class DecimalNumberMatcher {
     try {
       number = new Decimal(value);
     } catch (e) {
-      result.addInvalidTypeError("doubleNumber.e001",
-          "The value is not a valid decimal number.");
+      throwError(ERRORS.INVALID_DECIMAL);
     }
     if (number?.precision(true) > maxDigits) {
-      result.addInvalidTypeError("doubleNumber.e002",
-          "The value exceeded maximum number of digits.");
+      throwError(ERRORS.DIGITS_EXCEEDED);
     }
     if (maxDigits && number?.decimalPlaces() > maxDecimals) {
-      result.addInvalidTypeError("doubleNumber.e003",
-          "The value exceeded maximum number of decimal places.");
+      throwError(ERRORS.DECIMALS_EXCEEDED);
     }
     return result;
   }
